ca46ce956a411abdceb333a37b12b9f2ab511caf
kolergy <kolergy@gmail.com>
2021-08-08 18:44:45 +0200

    improved

--- scripts/post_script.py
+++ scripts/post_script.py
@@ -2,19 +2,16 @@ from shutil import copyfile
 import os
 
 Import("env")
-#sou = "C:\\Users\\jmrog\\Documents\\Github\\IoToTa\\.pio\\build\\lolin_d32\\firmware.bin"
 sou = ".pio\\build\\lolin_d32\\firmware.bin"
-#dst = "C:\\Users\\jmrog\\Documents\Github\\IoToTa\\Data\\fw.bin"
 dst = "Data\\fw.bin"
 def after_build(source, target, env): 
-    print("[From Script] Finished building!!")
-    print("Source:{0}".format(source))
-    print("Target:{0}".format(target))
-    print("Env:   {0}".format(env   ))
-    path = os.getcwd()
-    print("Current path:{0}".format(path)) 
+    print("Finished building!! Copy the bin file in the Data directory")
+    #print("Source:{0}".format(source))
+    #print("Target:{0}".format(target))
+    #print("Env:   {0}".format(env   ))
+    #path = os.getcwd()
+    #print("Current path:{0}".format(path)) 
     #res = Copy(target, dst)
-    #res = Copy(sou, dst)
     res = copyfile(sou, dst)
     print(res)
     
