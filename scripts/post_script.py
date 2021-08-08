e686ecd88dd2f20667ef8049404671572c1a09ee
kolergy <kolergy@gmail.com>
2021-08-08 18:16:23 +0200

    post script

--- scripts/post_script.py
+++ scripts/post_script.py
@@ -2,7 +2,10 @@ from shutil import copyfile
 import os
 
 Import("env")
-
+#sou = "C:\\Users\\jmrog\\Documents\\Github\\IoToTa\\.pio\\build\\lolin_d32\\firmware.bin"
+sou = ".pio\\build\\lolin_d32\\firmware.bin"
+#dst = "C:\\Users\\jmrog\\Documents\Github\\IoToTa\\Data\\fw.bin"
+dst = "Data\\fw.bin"
 def after_build(source, target, env): 
     print("[From Script] Finished building!!")
     print("Source:{0}".format(source))
@@ -10,7 +13,10 @@ def after_build(source, target, env):
     print("Env:   {0}".format(env   ))
     path = os.getcwd()
     print("Current path:{0}".format(path)) 
-
-    #copyfile(src, dst)
+    #res = Copy(target, dst)
+    #res = Copy(sou, dst)
+    res = copyfile(sou, dst)
+    print(res)
+    
 
 env.AddPostAction("buildprog", after_build)
