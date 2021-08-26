61fcd2a98a82853d5dd1c474a25b3e9c26c68434
kolergy <kolergy@gmail.com>
2021-08-26 19:34:29 +0200

    post

--- scripts/post_script.py
+++ scripts/post_script.py
@@ -5,6 +5,7 @@ Import("env")
 sou = ".pio\\build\\lolin_d32\\firmware.bin"
 dst = "Data\\fw.bin"
 def after_build(source, target, env): 
+    """After Build, Autaumaticaly coppy the bin file to the daa dir then update the version file"""
     print("Finished building!! Copy the bin file in the Data directory")
     #print("Source:{0}".format(source))
     #print("Target:{0}".format(target))
@@ -12,8 +13,9 @@ def after_build(source, target, env):
     #path = os.getcwd()
     #print("Current path:{0}".format(path)) 
     #res = Copy(target, dst)
-    res = copyfile(sou, dst)
+    res = copyfile(sou, dst)  # copy the bin file TODO adapt to target
     print(res)
+    # TODO update the version file
     
 
 env.AddPostAction("buildprog", after_build)
