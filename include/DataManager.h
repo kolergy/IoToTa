d6c1d2fc88bf90d352bc7c11350b946d85f387d3
kolergy <kolergy@gmail.com>
2022-01-31 00:07:13 +0100

    mqtt

---
+++ include/DataManager.h
@@ -0,0 +1,45 @@
+#include <arduino.h>
+
+#ifndef DATA_MANAGER
+  #define DATA_MANAGER
+
+#include <stdlib.h>
+#include <wifi.h>
+#include <PubSubClient.h> // by Nick O'Leary
+
+class DataManager {
+
+    public:
+      DataManager(char* topic, char* server_IP, int server_port, bool debug=false, bool signature=true, bool numbering=true);
+      void dataConnect();
+      void pushData();
+
+    private:
+      bool          o_topic;
+      bool          o_debug;
+      bool          o_signature;
+      bool          o_numbering;
+      char*         o_server_IP;
+      int           o_server_port;
+      unsigned long o_numbering_counter;
+      WiFiClient   o_wifi_client;
+      PubSubClient o_mqtt_client;
+
+      void create_MQTT_client();
+      
+
+      void MQTTSend(char* topic, char* payload);
+      //void mongoConnect();
+      //void influxConnect();
+
+
+};
+  
+
+
+
+
+
+
+
+#endif // DATA_TO_CLOUD
--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -3,26 +3,41 @@
   #include "DataManager.h"
 #endif // DATA_Manager
 
-//#include <PubSubClient.h>
 
-DataManager::DataManager(char* topic, bool debug, bool signature, bool numbering) {
-   o_debug     = debug;
-   o_signature = signature;
-   o_numbering = numbering;
+
+DataManager::DataManager(char* topic, char* server_IP, int server_port, bool debug, bool signature, bool numbering) {
+  o_debug       = debug;
+  o_signature   = signature;
+  o_numbering   = numbering;
+  o_server_IP   = server_IP;
+  o_server_port = server_port;
   if(o_debug) Serial.print("Creating the DataManager DM object");
-
+  if(o_numbering == true) {
+    o_numbering_counter = 0;
+  }
+  create_MQTT_client();
+}
+void DataManager::create_MQTT_client() {
+  if(o_debug) Serial.print("Creating the MQTT client");
+  o_wifi_client = WiFiClient();
+  o_mqtt_client = PubSubClient(o_wifi_client);
 }
 
-void DataManager::MQTTConnect() {
-  if(o_debug) Serial.print("DM MQTTConnect Not implemented Yet");
+void DataManager::MQTTsend(char* topic, char* payload) {
+  // sends the data to the MQTT broker
+  if(o_debug) Serial.print("Sending data to MQTT broker");
+  if(o_numbering == true) {
+    o_numbering_counter++;
+  }
+  o_mqtt_client.publish(topic, payload);
 }
 
 
-void DataManager::mongoConnect() {
-  if(o_debug) Serial.print("DM mongoConnect Not implemented Yet");
-}
+//void DataManager::mongoConnect() {
+//  if(o_debug) Serial.print("DM mongoConnect Not implemented Yet");
+//}
 
 
-void DataManager::influxConnect() {
-  if(o_debug) Serial.print("DM influxConnect Not implemented Yet");
-}
+//void DataManager::influxConnect() {
+//  if(o_debug) Serial.print("DM influxConnect Not implemented Yet");
+//}
